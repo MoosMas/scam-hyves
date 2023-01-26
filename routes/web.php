bf0b23c4373cced518ad42f1865fecc4bb230d79
jenniferwillems <70897990+jenniferwillems@users.noreply.github.com>
2023-01-26 16:07:24 +0100

    simple seeder category and item

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,16 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    $posts = \App\Models\Post::all();
+    $posts = \App\Models\Post::find(1);
     return view('dashboard', ['posts' => $posts]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::resource('post', PostController::class);
-    
+
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
