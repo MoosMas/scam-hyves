144c70de93d5c23e6eeb43c165fd5dd2551a8a3b
Sam Brands <d286050@edu.rocwb.nl>
2023-01-26 08:21:15 +0100

    Removed font comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,7 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-{{--        <style>--}}
-{{--            body {--}}
-{{--                font-family: 'Nunito', sans-serif;--}}
-{{--            }--}}
-{{--        </style>--}}
+        
     </head>
     <body class="antialiased">
         
